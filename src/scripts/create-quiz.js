945a017460be72a4c5ba76ff27d25e52b38c8ab8
Richard <richard.herew@gmail.com>
2020-12-25 22:50:59 +0700

    Finished styling list of questions in create-quiz page and added the functionality to add and recod new questions in JS

--- src/scripts/create-quiz.js
+++ src/scripts/create-quiz.js
@@ -1,8 +1,80 @@
+const MDCRipple = require('@material/ripple').MDCRipple;
+const ol = document.querySelector('ol.create-quiz__list');
+const questions = [];
+
 function run(deck) {
-    const listButton = document.querySelector('#create__list-button');
-    listButton.addEventListener('click', () => {
-        deck.slide(4);
+    const addQuestionButton = document.querySelector('#create-quiz__add-question-button');
+    const buttonRipple = new MDCRipple(addQuestionButton);
+    initializeQuestionList();
+    addQuestionButton.addEventListener('click', e => {
+        addQuestion();
+    });
+}
+
+function createNewEmptyQuestion() {
+    return {
+        'number': questions.length + 1,
+        'question': '',
+        'timer': 20,
+        'options': ['', '', '', '']
+    }
+}
+
+function initializeQuestionList() {
+    addQuestion();
+}
+
+function addQuestion() {
+    const newQuestion = createNewEmptyQuestion();
+    const newLi = createQuestionPreviewLi(newQuestion);
+    ol.append(newLi);
+    questions.push(newQuestion);
+    addListPreviewListener(newLi);
+}
+
+function createQuestionPreviewLi(newQuestion) {
+    removeCurrentActiveListPreview();
+
+    const li = document.createElement('li');
+    li.classList.add('create-quiz__list__item');
+    li.classList.add('create-quiz__list__item--active');
+    li.innerHTML = `
+        <div class="create-quiz__list__item--left">
+            <p>${newQuestion.number}</p>
+            <img src="https://i.imgur.com/etf85Lw.png" alt="Delete">
+        </div>
+        <div class="create-quiz__list__item--right">
+            <p>Quiz</p>
+            <div>
+                <p class="create-quiz__list__item--right__question">
+                    ${newQuestion.question ? newQuestion.question : "Type your question"}
+                </p>
+                <p class="create-quiz__list__item--right__timer">
+                    ${newQuestion.timer}
+                </p>
+                <div class="create-quiz__list__item--right__options">
+                    <div>${newQuestion.options[0]}</div>
+                    <div>${newQuestion.options[1]}</div>
+                    <div>${newQuestion.options[2]}</div>
+                    <div>${newQuestion.options[3]}</div>
+                </div>
+            </div>
+        </div>
+    `
+
+    return li;
+}
+
+function addListPreviewListener(listPreview) {
+    listPreview.addEventListener('click', e => {
+        removeCurrentActiveListPreview();
+        listPreview.classList.add('create-quiz__list__item--active');
     })
+}
+
+function removeCurrentActiveListPreview() {
+    const activeLi = document.querySelector('.create-quiz__list__item--active');
+    if (activeLi) activeLi.classList.remove('create-quiz__list__item--active');
 }
 
 module.exports = {
