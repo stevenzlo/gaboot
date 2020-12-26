c19cf18d144dd847fbde263dd372c6d041e0cb56
Steven <hikarizlo122@gmail.com>
2020-12-26 23:53:33 +0700

    reconfigure quiz structure in firestore, sync quiz list with firestore structure

--- src/scripts/quiz-list.js
+++ src/scripts/quiz-list.js
@@ -8,35 +8,35 @@ function run(deck) {
 function initQuizList(deck) {
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-      const docRef = firebase.firestore().collection("users").doc(user.uid);
-      docRef.onSnapshot(function(doc) {
+      const docRef = firebase.firestore().collection('users').doc(user.uid).collection('quiz');
+      docRef.onSnapshot(function(querySnapshot) {
         const quizListBody = document.getElementById('quiz-list-body');
         quizListBody.innerHTML = "";
-        if (doc.exists) {
-          const quizArray = doc.data()['quiz'];
-          if (quizArray && quizArray.length > 0) {
-            quizArray.forEach((data, i) => {
-              const tr = document.createElement('tr');
-              tr.innerHTML = `
-                <tr class="mdc-data-table__row">
-                  <th class="mdc-data-table__cell" scope="row">#${i+1}</th>
-                  <td class="mdc-data-table__cell">${data['title']}</td>
-                  <td class="mdc-data-table__cell">${data['description']}</td>
-                  <td class="mdc-data-table__cell">${data['playsCount']}</td>
-                  <td class="mdc-data-table__cell">
-                    <button class="mdc-button mdc-button--raised" data-id="${data['id']}">
-                      <span class="mdc-button__label">Play</span>
-                    </button>
-                  </td>
-                  <td class="mdc-data-table__cell">
-                    <button class="mdc-button mdc-button--raised" data-id="${data['id']}">
-                      <span class="mdc-button__label">Edit</span>
-                    </button>
-                  </td>
-                </tr>`;
-              quizListBody.append(tr);
-            })
-          }
+        if (querySnapshot.size) {
+          let i = 1;
+          querySnapshot.forEach((doc) => {
+            const tr = document.createElement('tr');
+            const data = doc.data();
+            tr.innerHTML = `
+              <tr class="mdc-data-table__row">
+                <th class="mdc-data-table__cell" scope="row">#${i}</th>
+                <td class="mdc-data-table__cell">${data['title']}</td>
+                <td class="mdc-data-table__cell">${data['description']}</td>
+                <td class="mdc-data-table__cell">${data['playsCount']}</td>
+                <td class="mdc-data-table__cell text-center">
+                  <button class="mdc-button mdc-button--raised" data-id="${doc.id}">
+                    <span class="mdc-button__label">Host</span>
+                  </button>
+                </td>
+                <td class="mdc-data-table__cell text-center">
+                  <button class="mdc-button mdc-button--raised" data-id="${doc.id}">
+                    <span class="mdc-button__label">Edit</span>
+                  </button>
+                </td>
+              </tr>`;
+            quizListBody.append(tr);
+            i++;
+          })
         } else {
           quizListBody.innerHTML = `
             <tr class="mdc-data-table__row">
